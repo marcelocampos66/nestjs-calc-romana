5eb72f4d3704d41699d498a6a711436769ae61bd
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-11-22 13:36:23 -0300

    Fiz o controller e moldei o service de Users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { User } from './User';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private UsersService: UsersService) {}
+
+  @Post()
+  async register(@Body() user: User) {
+    return this.UsersService.register(user);
+  }
+
   @Get()
-  findAll(): string {
-    return 'Teste!';
+  async getAll() {
+    return this.UsersService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return this.UsersService.getById(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() user: User) {
+    return this.UsersService.updateUser(id, user);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    await this.UsersService.deleteUser(id);
+    return { message: 'User succesfully deleted' };
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './User';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  register(user: User) {
+    return user;
+  }
+
+  getAll() {
+    return [];
+  }
+
+  getById(id: string) {
+    return id;
+  }
+
+  updateUser(id: string, user: User) {
+    return { id, ...user };
+  }
+
+  deleteUser(id: string) {
+    return id;
+  }
+}
